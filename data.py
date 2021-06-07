37dbafbad22bca28561508eaa702391d46ef9baa
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-06-07 15:02:56 -0700

    initial commit

---
+++ data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+
+'''includes US stock symbols with market cap > 100 Million, and price above $3. 
+Download the csv file:
+https://www.nasdaq.com/market-activity/stocks/screener?exchange=nasdaq&letter=0&render=download 
+for all of the NYSE, NASDAQ and NYSEAMERICAN public traded companies.
+'''
+
+stocks = pd.read_csv('tickers.csv')
+
+# Last Sale column is an object, let's convert to float
+stocks['Last Sale'] = stocks['Last Sale'].str.replace('$', '')
+stocks['Last Sale'] = pd.to_numeric(stocks['Last Sale'], downcast='float')
+type(stocks['Last Sale'][0])
+
+# Filter out stocks >$3 and > $100 million cap
+price_filter = stocks['Last Sale'] >= 3.00
+cap_filter = stocks['Market Cap'] >= 100000000
+
+# make set of symbols
+stocks = set(stocks[(price_filter) & (cap_filter)]['Symbol'])
+
+# Includes common words and words used on wsb that are also stock names
+blacklist = {'I', 'ELON', 'WSB', 'THE', 'A', 'ROPE', 'YOLO', 'TOS', 'CEO', 'DD', 'IT', 'OPEN', 'ATH', 'PM', 'IRS', 'FOR','DEC', 'BE', 'IMO', 'ALL', 'RH', 'EV', 'TOS', 'CFO', 'CTO', 'DD', 'BTFD', 'WSB', 'OK', 'PDT', 'RH', 'KYS', 'FD', 'TYS', 'US', 'USA', 'IT', 'ATH', 'RIP', 'BMW', 'GDP', 'OTM', 'ATM', 'ITM', 'IMO', 'LOL', 'AM', 'BE', 'PR', 'PRAY', 'PT', 'FBI', 'SEC', 'GOD', 'NOT', 'POS', 'FOMO', 'TL;DR', 'EDIT', 'STILL', 'WTF', 'RAW', 'PM', 'LMAO', 'LMFAO', 'ROFL', 'EZ', 'RED', 'BEZOS', 'TICK', 'IS', 'PM', 'LPT', 'GOAT', 'FL', 'CA', 'IL', 'MACD', 'HQ', 'OP', 'PS', 'AH', 'TL', 'JAN', 'FEB', 'JUL', 'AUG', 'SEP', 'SEPT', 'OCT', 'NOV', 'FDA', 'IV', 'ER', 'IPO', 'MILF', 'BUT', 'SSN', 'FIFA', 'USD', 'CPU', 'AT', 'GG', 'Mar'}
+
+# Adding wsb/reddit flavour to vader to improve sentiment analysis, score: 4.0 to -4.0
+new_words = {
+    'citron': -4.0,  
+    'hidenburg': -4.0,        
+    'moon': 4.0,
+    'highs': 2.0,
+    'mooning': 4.0,
+    'long': 2.0,
+    'short': -2.0,
+    'call': 4.0,
+    'calls': 4.0,    
+    'put': -4.0,
+    'puts': -4.0,    
+    'break': 2.0,
+    'tendie': 2.0,
+     'tendies': 2.0,
+     'town': 2.0,     
+     'overvalued': -3.0,
+     'undervalued': 3.0,
+     'buy': 4.0,
+     'sell': -4.0,
+     'gone': -1.0,
+     'gtfo': -1.7,
+     'paper': -1.7,
+     'bullish': 3.7,
+     'bearish': -3.7,
+     'bagholder': -1.7,
+     'stonk': 1.9,
+     'green': 1.9,
+     'money': 1.2,
+     'print': 2.2,
+     'rocket': 2.2,
+     'bull': 2.9,
+     'bear': -2.9,
+     'pumping': -1.0,
+     'sus': -3.0,
+     'offering': -2.3,
+     'rip': -4.0,
+     'downgrade': -3.0,
+     'upgrade': 3.0,     
+     'maintain': 1.0,          
+     'pump': 1.9,
+     'hot': 1.5,
+     'drop': -2.5,
+     'rebound': 1.5,  
+     'crack': 2.5,}
---
+++ update-financials.py
@@ -0,0 +1,85 @@
+from os import set_blocking
+import requests
+import pandas as pd
+import numpy as np
+from datetime import date
+import matplotlib.pyplot as plt
+
+# Load in df
+df = pd.read_csv('df.csv', index_col=0)
+
+# Request info from TD Ameritrade API
+client_id = "***REMOVED***"
+stocks_list = list(df['stock'])
+
+# The Quotes api call retrieves ticker information (price, etc.)
+# The Fundamentals api call retrieves company information (market cap, p/e ratio, etc.)
+# parameters
+parameters_quotes = {
+    'apikey': client_id,
+    'symbol': stocks_list,
+}
+
+parameters_fund = {
+    'apikey': client_id,
+    'symbol': stocks_list,
+    'projection': 'fundamental'
+}
+
+quotes_url = f'https://api.tdameritrade.com/v1/marketdata/quotes?apikey={client_id}'
+fundamental_url = f'https://api.tdameritrade.com/v1/instruments?apikey={client_id}'
+
+data_quotes = requests.get(url = quotes_url, params = parameters_quotes).json()
+data_fundamental = requests.get(url = fundamental_url, params = parameters_fund).json()
+
+# quotes dataframe
+df_q = pd.DataFrame.from_dict(data_quotes, orient = 'index')
+df_q.reset_index(inplace=True)
+df_q.drop('index', axis=1, inplace=True)
+
+# Establish fund columns for datafrane
+fund_cols = []
+for column in [x for x in [*data_fundamental[stocks_list[0]]['fundamental']]]:
+    fund_cols.append(column)
+
+# Append data to dataframe
+df_f = pd.DataFrame(columns = fund_cols)
+for stock in stocks_list:
+    df_f = df_f.append(pd.Series(data_fundamental[stock]['fundamental'], index=fund_cols), ignore_index=True)
+
+
+# Combine both dataframes
+# Remove duplicates
+current = pd.concat([df, df_q, df_f], axis=1)
+current = current.loc[:, ~current.columns.duplicated()]
+
+# Read csv
+historic_sentiment_analysis = pd.read_csv('historic_sentiment_analysis.csv')
+#historic_sentiment_analysis = historic_sentiment_analysis.iloc[:, 1:]
+
+# Remove duplicates
+#historic_sentiment_analysis = historic_sentiment_analysis.loc[:, ~historic_sentiment_analysis.duplicated()]
+
+# Update csv
+historic_sentiment_analysis = pd.concat([historic_sentiment_analysis, current], axis = 0, ignore_index=True)
+#historic_sentiment_analysis.to_csv('historic_sentiment_analysis.csv', index=False)
+
+
+####################################################################################
+
+
+# current_doubles = []
+# for i in range(len(current.T.duplicated())):
+#     if current.T.duplicated()[i]:
+#         current_doubles.append(i)
+
+# current.iloc[:, 9]
+# current.loc[:,'symbol']
+# historic_sentiment_analysis.loc[:, 'symbol']
+# historic_doubles = []
+# for i in range(len(historic_sentiment_analysis.T.duplicated())):
+#     if historic_sentiment_analysis.T.duplicated()[i]:
+#         historic_doubles.append(i)
+
+# print(len(current_doubles))
+# print(len(historic_doubles))
