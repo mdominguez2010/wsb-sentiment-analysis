dba371ad374f87cc8f4d6c395651038190c05463
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-06-08 13:12:39 -0700

    update files

--- data.py
+++ data.py
@@ -24,7 +24,7 @@ stocks = set(stocks[(price_filter) & (cap_filter)]['Symbol'])
 # Includes common words and words used on wsb that are also stock names
 blacklist = {'I', 'ELON', 'WSB', 'THE', 'A', 'ROPE', 'YOLO', 'TOS', 'CEO', 'DD', 'IT', 'OPEN', 'ATH', 'PM', 'IRS', 'FOR','DEC', 'BE', 'IMO', 'ALL', 'RH', 'EV', 'TOS', 'CFO', 'CTO', 'DD', 'BTFD', 'WSB', 'OK', 'PDT', 'RH', 'KYS', 'FD', 'TYS', 'US', 'USA', 'IT', 'ATH', 'RIP', 'BMW', 'GDP', 'OTM', 'ATM', 'ITM', 'IMO', 'LOL', 'AM', 'BE', 'PR', 'PRAY', 'PT', 'FBI', 'SEC', 'GOD', 'NOT', 'POS', 'FOMO', 'TL;DR', 'EDIT', 'STILL', 'WTF', 'RAW', 'PM', 'LMAO', 'LMFAO', 'ROFL', 'EZ', 'RED', 'BEZOS', 'TICK', 'IS', 'PM', 'LPT', 'GOAT', 'FL', 'CA', 'IL', 'MACD', 'HQ', 'OP', 'PS', 'AH', 'TL', 'JAN', 'FEB', 'JUL', 'AUG', 'SEP', 'SEPT', 'OCT', 'NOV', 'FDA', 'IV', 'ER', 'IPO', 'MILF', 'BUT', 'SSN', 'FIFA', 'USD', 'CPU', 'AT', 'GG', 'Mar'}
 
-# Adding wsb/reddit flavour to vader to improve sentiment analysis, score: 4.0 to -4.0
+# Adding wsb/reddit flavor to vader to improve sentiment analysis, score: 4.0 to -4.0
 new_words = {
     'citron': -4.0,  
     'hidenburg': -4.0,        
