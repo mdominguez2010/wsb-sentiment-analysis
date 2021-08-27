ead8c5b045d3fdeb06f983dec25b409bbf94d071
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-08-27 16:21:29 -0700

    initial commit

---
+++ model/model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import xgboost as xgb
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import auc, roc_auc_score, plot_roc_curve, confusion_matrix, ConfusionMatrixDisplay, precision_recall_curve, PrecisionRecallDisplay, plot_confusion_matrix, precision_score, recall_score
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
+
+def establish_features_target():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+
+    main()
