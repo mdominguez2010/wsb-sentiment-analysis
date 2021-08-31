87471c8e79f8ec55b9fa0a9fc525d80927cda134
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-08-31 10:44:33 -0700

    load data

--- model/model.py
+++ model/model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import pickle
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -14,11 +15,35 @@ from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt
 
-def establish_features_target():
-    pass
+def load_pickle(file_path):
+    ***REMOVED***
+    Loads pickled 'combined_df' file
+    ***REMOVED***
+
+    with open(file_path, 'rb') as f:
+        data = pickle.load(f)
+
+    return data
+
+def establish_features_target(features, target):
+    ***REMOVED***
+    Returns features and target in separate 
+    ***REMOVED***
+    X, y = features, target
+
+    return X, y
 
 def main():
-    pass
+    # Load data
+    combined_df = load_pickle('../data/combined_df.pickle')
+
+    # Establish features/target
+    X, y = establish_features_target(
+        combined_df.loc[:, 'Bearish':].drop('date', axis=1),
+        combined_df['5d-direction'])
+
+    print(X.head())
+    print(y.head())
 
 if __name__ == "__main__":
 
