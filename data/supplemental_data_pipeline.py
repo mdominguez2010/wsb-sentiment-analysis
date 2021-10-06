af1b47c5bf824eb2b6f4623d55ecc432f138fd4e
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-10-06 15:05:51 -0700

    update api call

--- data/supplemental_data_pipeline.py
+++ data/supplemental_data_pipeline.py
@@ -8,20 +8,20 @@ from secrets import api_key_alpha
 import datetime
 import time
 
-def make_api_call(params, stock):
-    url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey={api_key_alpha}'
-    r = requests.get(url, params=params)
-    data = r.json()
+def make_api_call(api_key, stock):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={stock}&apikey={api_key}'
+    r = requests.get(url)
+    print(r.status_code)
 
-    return data
+    return r
+
+def get_list_of_stocks(filepath):
+    dataframe = pd.read_csv(filepath)
+    list_of_stocks = list(dataframe['stock'].unique())
+
+    return list_of_stocks
 
 if __name__ == "__main__":
-    STOCK = 'TSLA'
-    PARAMS = {
-        'function': 'OVERVIEW',
-        'symbol': STOCK,
-        'apikey': api_key_alpha
-    }
+    list_of_stocks = get_list_of_stocks('../historic_sentiment_analysis.csv')[0]
 
-    data = make_api_call(params=PARAMS, stock=STOCK)
-    print(data)
+    for stock in list_of_stocks
