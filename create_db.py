99e3d40bb729fcf5f4740c104bb356aef3eca33d
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-10-13 16:34:48 -0700

    add tables

--- create_db.py
+++ create_db.py
@@ -1,2 +1,57 @@
-import sqlite3
-import alpaca_trade_api as trade_api
+import sqlite3, secrets
+
+# Connect to database
+connection = sqlite3.connect(secrets.DB_FILE_PATH)
+
+# Create cursor object
+cursor = connection.cursor()
+
+# Execute SQL commands
+cursor.execute(***REMOVED***
+    CREATE TABLE IF NOT EXISTS stock (
+        id INTEGER PRIMARY KEY, 
+        symbol TEXT NOT NULL UNIQUE, 
+        name TEXT NOT NULL
+    )
+***REMOVED***)
+
+cursor.execute(***REMOVED***
+    CREATE TABLE IF NOT EXISTS sentiment (
+        id INTEGER PRIMARY KEY, 
+        stock_id INTEGER,
+        date NOT NULL,
+        bearish REAL,
+        neutral REAL,
+        bullish REAL,
+        compound REAL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+***REMOVED***)
+
+cursor.execute(***REMOVED***
+    CREATE TABLE IF NOT EXISTS stock_price (
+        id INTEGER PRIMARY KEY, 
+        stock_id INTEGER,
+        date NOT NULL,
+        open NOT NULL, 
+        high NOT NULL, 
+        low NOT NULL, 
+        close NOT NULL,
+        volume NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+***REMOVED***)
+
+cursor.execute(***REMOVED***
+    CREATE TABLE IF NOT EXISTS fundamentals (
+        id INTEGER PRIMARY KEY, 
+        stock_id INTEGER,
+        date NOT NULL,
+
+
+
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+***REMOVED***)
+
+connection.commit()
