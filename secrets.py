3743e889dcc11245183bcc8a1821474d09ed0e48
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-10-13 16:11:24 -0700

    add Alpaca api cred info

--- secrets.py
+++ secrets.py
@@ -1,13 +1,23 @@
 ***REMOVED***
-Credentials for Praw
+***REMOVED***
+***REMOVED***
+
 ***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
-***REMOVED***
-Credentials for TDAmeritrade Developer API
 ***REMOVED***
 
 api_key = "UAUJOIVBLRMYXNFIZKT3L9UFSUGZWPJ6"
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+DB_FILE_PATH = '/Users/dominguez/Documents/Trading-App2/app.db'
