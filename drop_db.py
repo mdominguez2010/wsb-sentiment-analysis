bc749031c0021109e9c454967ce297dcc1ad2269
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-10-13 17:34:57 -0700

    update tables

---
+++ drop_db.py
@@ -0,0 +1,32 @@
+***REMOVED***
+Deletes database tables.
+Useful when you want to start your db over from scratch
+***REMOVED***
+
+import sqlite3
+import secrets
+
+connection = sqlite3.connect(secrets.DB_FILE_PATH)
+    
+cursor = connection.cursor()
+
+cursor.execute(***REMOVED***
+    DROP TABLE stock
+***REMOVED***)
+
+cursor.execute(***REMOVED***
+    DROP TABLE sentiment
+***REMOVED***)
+
+cursor.execute(***REMOVED***
+    DROP TABLE stock_price
+***REMOVED***)
+
+cursor.execute(***REMOVED***
+    DROP TABLE fundamentals
+***REMOVED***)
+
+connection.commit()
+
+# Delete all stocks from stock database (does NOT delete the database)
+# cursor.execute("DELETE FROM stock")
