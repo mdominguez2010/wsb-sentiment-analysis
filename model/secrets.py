fc88886d28e8883b4bbb4f2368efc68600f5c1db
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-11-02 09:24:05 -0700

    add keys

--- model/secrets.py
+++ model/secrets.py
@@ -1,7 +1,23 @@
 """
 Keeps important information secret
 """
+################## r/WallStreetBets Creds ##################
 
-api_key = "***REMOVED***"
+user_agent = "Comment Extraction"
+client_id = "***REMOVED***"
+client_secret = "***REMOVED***"
 
-api_key_alpha = '***REMOVED***'
+################## TDAmeritrade API ##################
+
+API_KEY_TDA = "***REMOVED***"
+
+################## Alpaca Trade API ##################
+
+APCA_API_KEY_ID = '***REMOVED***'
+APCA_API_SECRET_KEY = '***REMOVED***'
+APCA_API_BASE_URL = 'https://api.alpaca.markets'
+DB_FILE_PATH = '***REMOVED***'
+
+################## Alpha Vantage ##################
+
+API_KEY_ALPHA_VANTAGE = '***REMOVED***'
