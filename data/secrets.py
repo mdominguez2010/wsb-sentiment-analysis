bfe785025ac8b1fd79234c0ab221927ae313da9d
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-11-04 15:27:11 -0700

    add new password to hide

--- data/secrets.py
+++ data/secrets.py
@@ -2,6 +2,6 @@
 Keeps important information secret
 """
 
-api_key = "***REMOVED***"
+api_key = "8U8IWEYTVSA0S7UPF0K1N18HCBHAMF7A"
 
-api_key_alpha = '***REMOVED***'
+api_key_alpha = 'VHOI1ERJ34C0FKHZ'
--- secrets.py
+++ secrets.py
@@ -4,20 +4,20 @@ Keeps important information secret
 ################## r/WallStreetBets Creds ##################
 
 user_agent = "Comment Extraction"
-client_id = "***REMOVED***"
-client_secret = "***REMOVED***"
+client_id = "QktIf3FZ10C5jg"
+client_secret = "52XN7-d-s7tBO8Wld9mPQ19PMaV8HA"
 
 ################## TDAmeritrade API ##################
 
-API_KEY_TDA = "***REMOVED***"
+API_KEY_TDA = "JLYIVLFHOX5M45STQFIGA3FJ4ASZ2MLB"
 
 ################## Alpaca Trade API ##################
 
-APCA_API_KEY_ID = '***REMOVED***'
-APCA_API_SECRET_KEY = '***REMOVED***'
+APCA_API_KEY_ID = 'AKBUXTGGS57T7MYMMPS7'
+APCA_API_SECRET_KEY = 'L8yoJ7u6bH5MhzCV2esSDhiRb09C0IUe4neoZdI6'
 APCA_API_BASE_URL = 'https://api.alpaca.markets'
-DB_FILE_PATH = '***REMOVED***'
+DB_FILE_PATH = '/Users/dominguez/Documents/wsb-sentiment-analysis/trading.db'
 
 ################## Alpha Vantage ##################
 
-API_KEY_ALPHA_VANTAGE = '***REMOVED***'
+API_KEY_ALPHA_VANTAGE = 'VHOI1ERJ34C0FKHZ'
