214ab063cd792b075ab17f731cf94b99901ff110
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-11-10 09:59:59 -0800

    update file

--- model/secrets.py
+++ model/secrets.py
@@ -3,21 +3,21 @@ Keeps important information secret
 """
 ################## r/WallStreetBets Creds ##################
 
-user_agent = "Comment Extraction"
-client_id = "QktIf3FZ10C5jg"
-client_secret = "52XN7-d-s7tBO8Wld9mPQ19PMaV8HA"
+user_agent = ""
+client_id = ""
+client_secret = ""
 
 ################## TDAmeritrade API ##################
 
-API_KEY_TDA = "JLYIVLFHOX5M45STQFIGA3FJ4ASZ2MLB"
+API_KEY_TDA = ""
 
 ################## Alpaca Trade API ##################
 
-APCA_API_KEY_ID = 'AKBUXTGGS57T7MYMMPS7'
-APCA_API_SECRET_KEY = 'L8yoJ7u6bH5MhzCV2esSDhiRb09C0IUe4neoZdI6'
-APCA_API_BASE_URL = 'https://api.alpaca.markets'
-DB_FILE_PATH = '/Users/dominguez/Documents/wsb-sentiment-analysis/trading.db'
+APCA_API_KEY_ID = ''
+APCA_API_SECRET_KEY = ''
+APCA_API_BASE_URL = ''
+DB_FILE_PATH = ''
 
 ################## Alpha Vantage ##################
 
-API_KEY_ALPHA_VANTAGE = 'VHOI1ERJ34C0FKHZ'
+API_KEY_ALPHA_VANTAGE = ''
